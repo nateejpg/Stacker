d1513a3eced99a5260537162bba4abdffa2d3a69
Natejpg <nathan11.abreu@gmail.com>
2024-03-30 16:17:12 -0300

    Deploy 20

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 })
 
 const db = mysql2.createConnection({
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     database: "mytodolist",
     user: "root",
     password: "nath26an"
