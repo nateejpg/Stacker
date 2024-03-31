9a2ae82d3ae9b378aabbb0f2d0ddfa591c0216cd
Natejpg <nathan11.abreu@gmail.com>
2024-03-31 12:20:45 -0300

    aa

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.listen(8800, () => {
+    console.log("backend is up!")
+})
+
+app.get("/", (req, res) => {
+    res.json("Welcome to the backend")
+})
+
 const db = mysql.createConnection({
     host: "localhost",
     database: "stacker",
@@ -15,13 +24,6 @@ const db = mysql.createConnection({
     password: "nath26an",
 })
 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-   
-  app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-  })
 
 // Default Tasks
 
