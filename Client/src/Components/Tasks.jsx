dc4e7ffbb75bd09ed4da9747f3d0aa177b947bd9
Natejpg <nathan11.abreu@gmail.com>
2024-03-31 14:26:57 -0300

    Adding Path

--- Client/src/Components/Tasks.jsx
+++ Client/src/Components/Tasks.jsx
@@ -41,7 +41,7 @@ const Tasks = () => {
   const fetchDefault = async () => {
 
      try{
-          const response = await fetch("https://stacker-server.vercel.app");
+          const response = await fetch("https://stacker-server.vercel.app/default");
           const data = await response.json();
 
           setDefaultA(data);
