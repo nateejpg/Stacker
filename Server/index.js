946681482760db1112b010b8d226e0ac8b5a5cc5
Natejpg <nathan11.abreu@gmail.com>
2024-03-31 12:18:39 -0300

    test

--- Server/index.js
+++ Server/index.js
@@ -8,15 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.listen(8800, () => {
-    console.log("backend is up!")
-})
-
-app.get("/", (req, res) => {
-    res.json("Welcome to the backend")
-})
-
 const db = mysql.createConnection({
     host: "localhost",
     database: "stacker",
